48ba6f33c6f2e12746559be0dee45aeea1e82c8f
amankumar12602 <amankumarsingh2803@gmail.com>
2021-01-24 18:40:05 +0530

    Delete UI directory

--- UI/src/app/auth/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
-import { NonAuthGuardService } from '../services/non-auth-guard.service';
-
-export const AuthRoutes = [
-  { path: 'login', component: SigninComponent, canActivate: [NonAuthGuardService] },
-  { path: 'signup', component: SignupComponent, canActivate: [NonAuthGuardService] }
- 
-];
--- UI/src/app/product-detail/product-detail.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductDetailComponent } from './product-detail.component';
-
-
-export const ProductDetailRoutes: Routes = [
-  { path: 'detail/:productUrl', component: ProductDetailComponent },
-  { path: 'detail/:productUrl/:variant', component: ProductDetailComponent }
-];
-
